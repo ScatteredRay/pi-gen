0af02e0f046ef4d1fae136696c120e47d9cedc70
Nicholas "Indy" Ray <Arelius@gmail.com>
2018-12-05 15:16:33 -0800

    Initial support for an image for a viewer.

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -4,12 +4,20 @@ install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
+user_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})
+root_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})
+# Write passwords to a file.
+cat <<EOF > /pi-gen/deploy/users
+nd ${user_passwd}
+root ${root_passwd}
+EOF
+
 on_chroot << EOF
 if ! id -u pi >/dev/null 2>&1; then
-	adduser --disabled-password --gecos "" pi
+	adduser --disabled-password --gecos "" nd
 fi
-echo "pi:raspberry" | chpasswd
-echo "root:root" | chpasswd
+echo "nd:${user_password}" | chpasswd
+echo "root:${root_password}" | chpasswd
 EOF
 
 
