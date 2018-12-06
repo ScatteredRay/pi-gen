5d839862e9a05d4e2c7d5d5c11170881bc52d979
Nicholas "Indy" Ray <Arelius@gmail.com>
2018-12-05 18:09:59 -0800

    Build fixes.

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -4,8 +4,8 @@ install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
-user_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})
-root_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})
+user_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c8)
+root_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c8)
 # Write passwords to a file.
 cat <<EOF > /pi-gen/deploy/users
 nd ${user_passwd}
@@ -16,8 +16,8 @@ on_chroot << EOF
 if ! id -u pi >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" nd
 fi
-echo "nd:${user_password}" | chpasswd
-echo "root:${root_password}" | chpasswd
+echo "nd:${user_passwd}" | chpasswd
+echo "root:${root_passwd}" | chpasswd
 EOF
 
 
