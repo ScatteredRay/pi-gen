74548206f308be876157ef8c0a17c8395723ff80
Nicholas "Indy" Ray <Arelius@gmail.com>
2019-10-29 20:54:47 -0700

    Fix wrong pass variable.

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -14,7 +14,7 @@ on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
 fi
-echo "${FIRST_USER_NAME}:${user_passwd}" | chpasswd
+echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
 echo "root:${ROOT_PASS}" | chpasswd
 EOF
 
