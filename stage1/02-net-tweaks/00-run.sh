9cda609d318c84cb19254e2701059066d51555c0
Indy Ray <arelius@gmail.com>
2020-07-27 13:26:17 -0700

    Moving hostname gen into it's own pass in stage2

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-#install -m 644 files/hostname "${ROOTFS_DIR}/etc/hostname"
-install -m 644 files/hostnamecp.service "${ROOTFS_DIR}/etc/systemd/system/hostnamecp.service"
-install -m 644 files/hostnamegen.service "${ROOTFS_DIR}/etc/systemd/system/hostnamegen.service"
-
-on_chroot << EOF
-systemctl enable hostnamecp
-systemctl enable hostnamegen
-EOF
+install -m 644 files/hostname "${ROOTFS_DIR}/etc/hostname"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
